d50a693ba5225402542ed35670dc50a14daf59b6
Beellal97242599@ <mahamanehabiboubilal999@gmail.com>
2023-10-26 16:54:53 +0100

    Update

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,9 +4,9 @@ import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
 import boy from "../../img/boy.png";
 import glassesimoji from "../../img/glassesimoji.png";
-import thumbup from "../../img/thumbup.png";
-import crown from "../../img/crown.png";
-import FloatinDiv from "../FloatingDiv/FloatingDiv";
+// import thumbup from "../../img/thumbup.png";
+// import crown from "../../img/crown.png";
+// import FloatinDiv from "../FloatingDiv/FloatingDiv";
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
@@ -49,8 +49,8 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
-        {/* animation */}
+        <img src={boy} alt="" /> 
+        
         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
